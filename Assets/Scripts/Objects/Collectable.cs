b9474c0ea0a3d0276749f5d3fe316f72b729876b
unknown <notquotemark@gmail.com>
2023-10-05 21:30:12 -0700

    fixed collectable collision

--- Assets/Scripts/Objects/Collectable.cs
+++ Assets/Scripts/Objects/Collectable.cs
@@ -18,7 +18,7 @@ public class Collectable : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
     }
 }
