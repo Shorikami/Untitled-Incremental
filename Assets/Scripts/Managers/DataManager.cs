6d8979d857d4e5a00ba15adce051aefdaef8f437
unknown <notquotemark@gmail.com>
2023-10-10 19:24:43 -0700

    better functionality for adding new currencies/experience/upgrades

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -35,6 +35,66 @@ public class DataManager : MonoBehaviour
     {
     }
 
+    public void NewCurrency(StatsManager.GameCurrencyType currtype, Collectable.CollectableType collType)
+    {
+        GameObject gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
+        gO.AddComponent<GameCurrency>();
+        GameCurrency gc = gO.GetComponent<GameCurrency>();
+
+        gc.m_Currency = new CurrencyData();
+        gc.m_Currency.m_CurrencyType = currtype;
+        gc.m_Currency.m_CollectableType = collType;
+        gc.m_Currency.m_TotalValue = 0;
+
+        m_GameData.m_GameCurrencies.Add(gc.m_Currency);
+        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
+    }
+
+    public void NewExperience(StatsManager.GameCurrencyType currtype, Collectable.CollectableType colltype,
+        StatsManager.GameCurrencyType perktype)
+    {
+        GameObject gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
+        gO.AddComponent<Experience>();
+        Experience ex = gO.GetComponent<Experience>();
+
+        ex.m_ExpData = new ExperienceData();
+        ex.m_ExpData.m_CurrencyType = currtype;
+        ex.m_ExpData.m_CollectableType = colltype;
+        ex.m_ExpData.m_PerkType = perktype;
+        ex.m_ExpData.m_TotalValue = 0;
+
+        m_GameData.m_ExperienceTypes.Add(ex.m_ExpData);
+        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
+    }
+
+    public void NewUpgrade(string name, string desc, int maxLevel, double baseCost, bool staticCost, int bonusReqLevels,
+        float levelBonusMulti, float bonusMulti, StatsManager.GameCurrencyType currtype, StatsManager.GameCurrencyType boughtWith,
+        StatsManager.NonCurrencyUpgrades nonCurrType, Collectable.CollectableType colltype)
+    {
+        GameObject gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
+        gO.AddComponent<Upgrade>();
+        Upgrade up = gO.GetComponent<Upgrade>();
+
+        up.m_UpgradeData = new UpgradeData();
+        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
+        up.m_UpgradeData.m_UpgradeName = name;
+        up.m_UpgradeData.m_Description = desc;
+        up.m_UpgradeData.m_CurrLevel = 0;
+        up.m_UpgradeData.m_MaxLevel = maxLevel;
+        up.m_UpgradeData.m_BaseCost = baseCost;
+        up.m_UpgradeData.m_StaticCost = staticCost;
+        up.m_UpgradeData.m_UpgradeBonuses.m_RequiredLevels = bonusReqLevels;
+        up.m_UpgradeData.m_UpgradeBonuses.m_LevelBonus = levelBonusMulti;
+        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = bonusMulti;
+        up.m_UpgradeData.m_CurrencyType = currtype;
+        up.m_UpgradeData.m_BoughtWith = boughtWith;
+        up.m_UpgradeData.m_NonCurrType = nonCurrType;
+        up.m_UpgradeData.m_CollectableType = colltype;
+
+        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
+        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
+    }
+
     public void NewGame()
     {
         // TODO: When starting completely fresh, the following will always be available from the start:
@@ -47,214 +107,36 @@ public class DataManager : MonoBehaviour
         // update this as more initial values are added
         m_GameData = new GameData();
 
-        GameObject gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<GameCurrency>();
-        GameCurrency gc = gO.GetComponent<GameCurrency>();
+        NewCurrency(StatsManager.GameCurrencyType.Coins, Collectable.CollectableType.Default);
+        NewCurrency(StatsManager.GameCurrencyType.Perks, Collectable.CollectableType.Default);
 
-        gc.m_Currency = new CurrencyData();
-        gc.m_Currency.m_CurrencyType = StatsManager.GameCurrencyType.Coins;
-        gc.m_Currency.m_CollectableType = Collectable.CollectableType.Default;
-        gc.m_Currency.m_Value = 0;
+        NewExperience(StatsManager.GameCurrencyType.Experience, Collectable.CollectableType.Default, StatsManager.GameCurrencyType.Perks);
 
-        m_GameData.m_GameCurrencies.Add(gc.m_Currency);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
+        NewUpgrade("Coins Value", "Increases value of coins by +25% per level. Coin value is doubled every 25 levels.",
+            500, 5, false, 25, 2.0f, 0.5f, StatsManager.GameCurrencyType.Coins, StatsManager.GameCurrencyType.Coins,
+            StatsManager.NonCurrencyUpgrades.Invalid, Collectable.CollectableType.Default);
+        NewUpgrade("Growth", "Increases spawn rate by +20% per level.",
+            100, 10, false, 0, 0.0f, 0.2f, StatsManager.GameCurrencyType.Coins, StatsManager.GameCurrencyType.Coins,
+            StatsManager.NonCurrencyUpgrades.GrowthRate, Collectable.CollectableType.Default);
+        NewUpgrade("XP Value", "Increases XP value by +25% per level. XP Value is doubled every 25 levels.",
+            500, 7, false, 25, 2.0f, 0.25f, StatsManager.GameCurrencyType.Experience, StatsManager.GameCurrencyType.Coins,
+            StatsManager.NonCurrencyUpgrades.Invalid, Collectable.CollectableType.Default);
+        NewUpgrade("Pickup Range+", "Increase pickup range by 10% per level.",
+            20, 10000, false, 0, 0.0f, 0.1f, StatsManager.GameCurrencyType.Coins, StatsManager.GameCurrencyType.Coins,
+            StatsManager.NonCurrencyUpgrades.PickupRange, Collectable.CollectableType.Default);
+        NewUpgrade("Speed+", "Increase movement speed by 10% per level.",
+            7, 1500, false, 0, 0.0f, 0.1f, StatsManager.GameCurrencyType.Coins, StatsManager.GameCurrencyType.Coins,
+            StatsManager.NonCurrencyUpgrades.MoveSpeed, Collectable.CollectableType.Default);
 
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<GameCurrency>();
-        gc = gO.GetComponent<GameCurrency>();
-
-        gc.m_Currency = new CurrencyData();
-        gc.m_Currency.m_CurrencyType = StatsManager.GameCurrencyType.Perks;
-        gc.m_Currency.m_CollectableType = Collectable.CollectableType.Default;
-        gc.m_Currency.m_Value = 0;
-
-        m_GameData.m_GameCurrencies.Add(gc.m_Currency);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Experience>();
-        Experience ex = gO.GetComponent<Experience>();
-
-        ex.m_ExpData = new ExperienceData();
-        ex.m_ExpData.m_CurrencyType = StatsManager.GameCurrencyType.Experience;
-        ex.m_ExpData.m_CollectableType = Collectable.CollectableType.Default;
-        ex.m_ExpData.m_PerkType = StatsManager.GameCurrencyType.Perks;
-        ex.m_ExpData.m_TotalValue = 0;
-
-        m_GameData.m_ExperienceTypes.Add(ex.m_ExpData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Upgrade>();
-        Upgrade up = gO.GetComponent<Upgrade>();
-
-        up.m_UpgradeData = new UpgradeData();
-        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
-        up.m_UpgradeData.m_UpgradeName = "Coins Value";
-        up.m_UpgradeData.m_Description = "Increases value of coins by +25% per level. Coin value is doubled every 25 levels.";
-        up.m_UpgradeData.m_CurrLevel = 0;
-        up.m_UpgradeData.m_MaxLevel = 500;
-        up.m_UpgradeData.m_BaseCost = 5;
-        up.m_UpgradeData.m_StaticCost = false;
-        up.m_UpgradeData.m_UpgradeBonuses.m_RequiredLevels = 25;
-        up.m_UpgradeData.m_UpgradeBonuses.m_LevelBonus = 2.0f;
-        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = 0.25f;
-        up.m_UpgradeData.m_CurrencyType = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_BoughtWith = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_NonCurrType = StatsManager.NonCurrencyUpgrades.Invalid;
-        up.m_UpgradeData.m_CollectableType = Collectable.CollectableType.Default;
-
-        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Upgrade>();
-        up = gO.GetComponent<Upgrade>();
-
-        up.m_UpgradeData = new UpgradeData();
-        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
-        up.m_UpgradeData.m_UpgradeName = "Growth";
-        up.m_UpgradeData.m_Description = "Increases spawn rate by +20% per level.";
-        up.m_UpgradeData.m_CurrLevel = 0;
-        up.m_UpgradeData.m_MaxLevel = 10;
-        up.m_UpgradeData.m_BaseCost = 10;
-        up.m_UpgradeData.m_StaticCost = false;
-        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = 0.20f;
-        up.m_UpgradeData.m_CurrencyType = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_BoughtWith = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_NonCurrType = StatsManager.NonCurrencyUpgrades.GrowthRate;
-        up.m_UpgradeData.m_CollectableType = Collectable.CollectableType.Default;
-        //StatsManager.m_Instance.UpdateMultiplier(up.m_UpgradeData, up.m_UpgradeData.m_UpgradeBonuses.m_CurrentBonus);
-
-        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Upgrade>();
-        up = gO.GetComponent<Upgrade>();
-
-        up.m_UpgradeData = new UpgradeData();
-        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
-        up.m_UpgradeData.m_UpgradeName = "XP Value";
-        up.m_UpgradeData.m_Description = "Increases XP value by +25% per level. XP Value is doubled every 25 levels.";
-        up.m_UpgradeData.m_CurrLevel = 0;
-        up.m_UpgradeData.m_MaxLevel = 500;
-        up.m_UpgradeData.m_BaseCost = 7;
-        up.m_UpgradeData.m_StaticCost = false;
-        up.m_UpgradeData.m_UpgradeBonuses.m_RequiredLevels = 25;
-        up.m_UpgradeData.m_UpgradeBonuses.m_LevelBonus = 2.0f;
-        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = 0.25f;
-        up.m_UpgradeData.m_CurrencyType = StatsManager.GameCurrencyType.Experience;
-        up.m_UpgradeData.m_BoughtWith = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_NonCurrType = StatsManager.NonCurrencyUpgrades.Invalid;
-        up.m_UpgradeData.m_CollectableType = Collectable.CollectableType.Default;
-
-        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Upgrade>();
-        up = gO.GetComponent<Upgrade>();
-
-        up.m_UpgradeData = new UpgradeData();
-        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
-        up.m_UpgradeData.m_UpgradeName = "Pickup Range+";
-        up.m_UpgradeData.m_Description = "Increase pickup range by 10% per level.";
-        up.m_UpgradeData.m_CurrLevel = 0;
-        up.m_UpgradeData.m_MaxLevel = 20;
-        up.m_UpgradeData.m_BaseCost = 10000;
-        up.m_UpgradeData.m_StaticCost = false;
-        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = 0.1f;
-        up.m_UpgradeData.m_CurrencyType = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_BoughtWith = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_NonCurrType = StatsManager.NonCurrencyUpgrades.PickupRange;
-        up.m_UpgradeData.m_CollectableType = Collectable.CollectableType.Default;
-
-        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Upgrade>();
-        up = gO.GetComponent<Upgrade>();
-
-        up.m_UpgradeData = new UpgradeData();
-        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
-        up.m_UpgradeData.m_UpgradeName = "Speed+";
-        up.m_UpgradeData.m_Description = "Increase movement speed by 10% per level.";
-        up.m_UpgradeData.m_CurrLevel = 0;
-        up.m_UpgradeData.m_MaxLevel = 10;
-        up.m_UpgradeData.m_BaseCost = 1500;
-        up.m_UpgradeData.m_StaticCost = false;
-        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = 0.1f;
-        up.m_UpgradeData.m_CurrencyType = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_BoughtWith = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_NonCurrType = StatsManager.NonCurrencyUpgrades.MoveSpeed;
-        up.m_UpgradeData.m_CollectableType = Collectable.CollectableType.Default;
-
-        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Upgrade>();
-        up = gO.GetComponent<Upgrade>();
-
-        up.m_UpgradeData = new UpgradeData();
-        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
-        up.m_UpgradeData.m_UpgradeName = "Perks Coins Value";
-        up.m_UpgradeData.m_Description = "Increases value of coins by +10% per level.";
-        up.m_UpgradeData.m_CurrLevel = 0;
-        up.m_UpgradeData.m_MaxLevel = 100;
-        up.m_UpgradeData.m_BaseCost = 1;
-        up.m_UpgradeData.m_StaticCost = true;
-        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = 0.1f;
-        up.m_UpgradeData.m_CurrencyType = StatsManager.GameCurrencyType.Coins;
-        up.m_UpgradeData.m_BoughtWith = StatsManager.GameCurrencyType.Perks;
-        up.m_UpgradeData.m_NonCurrType = StatsManager.NonCurrencyUpgrades.Invalid;
-        up.m_UpgradeData.m_CollectableType = Collectable.CollectableType.Default;
-
-        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Upgrade>();
-        up = gO.GetComponent<Upgrade>();
-
-        up.m_UpgradeData = new UpgradeData();
-        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
-        up.m_UpgradeData.m_UpgradeName = "Perks EXP Value";
-        up.m_UpgradeData.m_Description = "Increases value of EXP by +10% per level.";
-        up.m_UpgradeData.m_CurrLevel = 0;
-        up.m_UpgradeData.m_MaxLevel = 100;
-        up.m_UpgradeData.m_BaseCost = 1;
-        up.m_UpgradeData.m_StaticCost = true;
-        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = 0.1f;
-        up.m_UpgradeData.m_CurrencyType = StatsManager.GameCurrencyType.Experience;
-        up.m_UpgradeData.m_BoughtWith = StatsManager.GameCurrencyType.Perks;
-        up.m_UpgradeData.m_NonCurrType = StatsManager.NonCurrencyUpgrades.MoveSpeed;
-        up.m_UpgradeData.m_CollectableType = Collectable.CollectableType.Default;
-
-        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
-
-        gO = Instantiate(m_EmptyCurrencyPrefab, StatsManager.m_Instance.gameObject.transform);
-        gO.AddComponent<Upgrade>();
-        up = gO.GetComponent<Upgrade>();
-
-        up.m_UpgradeData = new UpgradeData();
-        up.m_UpgradeData.m_UpgradeBonuses = new UpgradeData.Bonus();
-        up.m_UpgradeData.m_UpgradeName = "Perks Speed+";
-        up.m_UpgradeData.m_Description = "Increase movement speed by 10% per level.";
-        up.m_UpgradeData.m_CurrLevel = 0;
-        up.m_UpgradeData.m_MaxLevel = 10;
-        up.m_UpgradeData.m_BaseCost = 1;
-        up.m_UpgradeData.m_UpgradeBonuses.m_BonusMultiplier = 0.1f;
-        up.m_UpgradeData.m_CurrencyType = StatsManager.GameCurrencyType.None;
-        up.m_UpgradeData.m_BoughtWith = StatsManager.GameCurrencyType.Perks;
-        up.m_UpgradeData.m_NonCurrType = StatsManager.NonCurrencyUpgrades.MoveSpeed;
-        up.m_UpgradeData.m_CollectableType = Collectable.CollectableType.Default;
-
-        m_GameData.m_AvailableUpgrades.Add(up.m_UpgradeData);
-        StatsManager.m_Instance.m_LoadedDataNodes.Add(gO);
+        NewUpgrade("Perks Coins Value", "Increases value of coins by +10% per level.",
+            100, 1, true, 0, 0.0f, 0.1f, StatsManager.GameCurrencyType.Coins, StatsManager.GameCurrencyType.Perks,
+            StatsManager.NonCurrencyUpgrades.Invalid, Collectable.CollectableType.Default);
+        NewUpgrade("Perks EXP Value", "Increases value of EXP by +10% per level.",
+            100, 1, true, 0, 0.0f, 0.1f, StatsManager.GameCurrencyType.Experience, StatsManager.GameCurrencyType.Perks,
+            StatsManager.NonCurrencyUpgrades.Invalid, Collectable.CollectableType.Default);
+        NewUpgrade("Perks Speed+", "Increase movement speed by 10% per level.",
+            3, 1, true, 0, 0.0f, 0.1f, StatsManager.GameCurrencyType.None, StatsManager.GameCurrencyType.Perks,
+            StatsManager.NonCurrencyUpgrades.MoveSpeed, Collectable.CollectableType.Default);
     }
 
     public void Save()
